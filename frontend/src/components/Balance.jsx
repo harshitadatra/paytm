b824bc8d5120afa8dacd10d6c7c868c6bba7747d
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2024-06-25 01:31:56 +0530

    feat: update balance

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,8 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const Balance = ({ value }) => {
+  const [userBalance, setUserBalance] = useState(10000);
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:3000/api/v1/account/balance",
+
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      )
+      .then((response) => {
+        setUserBalance(response.data.balance);
+      });
+  }, []);
   return (
     <div className="flex">
       <div className="font-bold text-lg">Your balance</div>
-      <div className="font-semibold ml-4 text-lg">Rs {value}</div>
+      <div className="font-semibold ml-4 text-lg">Rs {userBalance}</div>
     </div>
   );
 };
